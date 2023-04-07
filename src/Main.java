6d26f787c9cf312bd6af9d7b7a96a55775b83177
Dmitry Boychevsky <javashdrug@yandex.ru>
2023-04-07 18:59:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 3000;
+        int rublesPerMile = 20;
+        int bonus = ticketPrice / rublesPerMile;
+        System.out.println(bonus);
+
+    }
+}
